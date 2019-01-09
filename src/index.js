63b4f6f2f47e765356a8322c89e1e405fe01c630
t5556@abv.bg <Doomhamer96>
2019-01-09 17:23:28 +0200

    Deleting src defaults

--- src/index.js
+++ src/index.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FakeRest from "fakerest";
+import sinon from "sinon";
+import LoginForm from 'loginForm'
 
-class Login extends React.Component {
+let data = {
+    'authors': [
+        { id: 0, first_name: 'Leo', last_name: 'Tolstoi' },
+        { id: 1, first_name: 'Jane', last_name: 'Austen' }
+    ],
+    'books': [
+        { id: 0, author_id: 0, title: 'Anna Karenina' },
+        { id: 1, author_id: 0, title: 'War and Peace' },
+        { id: 2, author_id: 1, title: 'Pride and Prejudice' },
+        { id: 3, author_id: 1, title: 'Sense and Sensibility' }
+    ],
+    'settings': {
+        language: 'english',
+        preferred_format: 'hardback',
+    },
+    'users': [
+        { id: 0, username: 'Toni', password: 'Valev' },
+        { id: 1, username: 'admin', password: 'admin' }
+    ]
+};
+// initialize fake REST server
+let restServer = new FakeRest.Server();
+restServer.init(data);
+
+// use sinon.js to monkey-patch XmlHttpRequest
+let server = sinon.fakeServer.create();
+server.respondWith(restServer.getHandler());
+
+
+let req = new XMLHttpRequest();
+req.open("GET", "/users", false);
+req.send(null);
+this.state.credentials = JSON.parse(req.responseText);
+
+class App extends React.Component {
     render() {
         return (
-            <form>
-                <input type="text" />
-                <input type="password" />
-                <input type="submit"/>
-            </form>
+            <LoginForm credentials={this.state.credentials} />
         )
     }
 }
-
 ReactDOM.render(
-    <Login />,
+    <App />,
     document.getElementById('root')
 );
---
+++ src/loginForm.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import App from 'index'
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        if (event.target.type === 'text'){
+            this.setState({
+                username: event.target.value
+            })
+        }else if (event.target.type === 'password') {
+            this.setState({
+                password: event.target.value
+            })
+        }
+
+    }
+
+    handleSubmit(event) {
+
+        event.preventDefault();
+        for (let i = 0; i <= credentials.length - 1; i++) {
+            if (credentials[i].username === this.state.username && credentials[i].password === this.state.password) {
+                alert('Success');
+                return
+            }
+        }
+        alert('error');
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    Username:
+                    <input type="text" value={this.state.username} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Password:
+                    <input type="password"  value={this.state.password} onChange={this.handleChange} />
+                </label>
+                <input type="submit" onClick={this.handleSubmit}/>
+            </form>
+        )
+    }
+}
